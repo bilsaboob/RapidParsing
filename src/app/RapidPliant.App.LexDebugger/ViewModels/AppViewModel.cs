363c143b7e251848624dff0fa123ac2e11d6ee14
bilsaboob <bilsaboob@gmail.com>
2016-10-22 03:36:34 +0200

    Added names to Lex expressions in debugger UI

--- src/app/RapidPliant.App.LexDebugger/ViewModels/AppViewModel.cs
+++ src/app/RapidPliant.App.LexDebugger/ViewModels/AppViewModel.cs
@@ -12,12 +12,58 @@ namespace RapidPliant.App.LexDebugger.ViewModels
 {
     public class AppViewModel : ViewModel
     {
+        private List<string> _nextPatternAutoNames;
+
         public DebuggerLexDfaGraphViewModel LexGraphViewModel { get; set; }
         
         public AppViewModel()
         {
             LexPatterns = new ObservableCollection<LexPatternViewModel>();
+            _nextPatternAutoNames = new List<string>();
+
+            CreatePatternNames();
+
+            ConsumeNextPatternName();
         }
+
+        private void ConsumeNextPatternName()
+        {
+            NewLexPatternName = _nextPatternAutoNames.First();
+            _nextPatternAutoNames.RemoveAt(0);
+            NewLexPatternIsAutoName = true;
+        }
+
+        private void CreatePatternNames()
+        {
+            for (var i = 'A'; i < 'Z'; ++i)
+            {
+                _nextPatternAutoNames.Add(i.ToString());
+            }
+        }
+
+        public string NewLexPatternName
+        {
+            get { return get(() => NewLexPatternName); }
+            set
+            {
+                if (!_nextPatternAutoNames.Contains(value))
+                {
+                    if (NewLexPatternIsAutoName)
+                    {
+                        var prevVal = get(() => NewLexPatternName);
+                        if (!string.IsNullOrEmpty(prevVal) && !_nextPatternAutoNames.Contains(prevVal))
+                        {
+                            _nextPatternAutoNames.Insert(0, prevVal);
+                        }
+                    }
+                    NewLexPatternIsAutoName = false;
+                }
+
+                set(() => NewLexPatternName, value);
+            }
+        }
+
+        public bool NewLexPatternIsAutoName { get; set; }
 
         public string NewLexPattern
         {
@@ -39,7 +85,12 @@ namespace RapidPliant.App.LexDebugger.ViewModels
 
         public void AddPattern()
         {
-            var lexPatternVm = new LexPatternViewModel(NewLexPattern);
+            var lexPatternVm = new LexPatternViewModel(NewLexPattern, NewLexPatternName);
+            lexPatternVm.IsAutoName = NewLexPatternIsAutoName;
+            _nextPatternAutoNames.Remove(NewLexPattern);
+
+            ConsumeNextPatternName();
+
             LexPatterns.Add(lexPatternVm);
             LexGraphViewModel.RefreshLexPatterns();
 
@@ -50,6 +101,24 @@ namespace RapidPliant.App.LexDebugger.ViewModels
         {
             LexPatterns.Remove(lexPattern);
             LexGraphViewModel.RefreshLexPatterns();
+
+            if (NewLexPatternIsAutoName && !string.IsNullOrEmpty(NewLexPatternName))
+            {
+                if (!_nextPatternAutoNames.Contains(NewLexPatternName))
+                {
+                    _nextPatternAutoNames.Insert(0, NewLexPatternName);
+                }
+            }
+
+            if (lexPattern.IsAutoName)
+            {
+                if (!_nextPatternAutoNames.Contains(lexPattern.Name))
+                {
+                    _nextPatternAutoNames.Insert(0, lexPattern.Name);
+                }
+            }
+
+            ConsumeNextPatternName();
         }
 
         public void RefreshPattern(LexPatternViewModel lexPattern)
@@ -60,15 +129,28 @@ namespace RapidPliant.App.LexDebugger.ViewModels
 
     public class LexPatternViewModel : ViewModel
     {
-        public LexPatternViewModel(string pattern)
+        private string _initialName;
+
+        public LexPatternViewModel(string pattern, string name)
         {
             Pattern = pattern;
+            Name = name;
         }
+
+        public bool IsAutoName { get; set; }
 
         public string Name
         {
             get { return get(() => Name); }
-            set { set(() => Name, value); }
+            set
+            {
+                if (value != _initialName)
+                    IsAutoName = false;
+                else
+                    IsAutoName = true;
+
+                set(() => Name, value);
+            }
         }
 
         public string Pattern
